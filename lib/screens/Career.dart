acad9ee7fb34e945433cd0daae0731b588b456c7
ySall <hassan.faysall.2821@rupp.edu.kh>
2024-09-09 10:38:52 +0700

    Modify Career Screen to display as dialog

--- lib/screens/Career.dart
+++ lib/screens/Career.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:majorwhisper/screens/CareerResult.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:lottie/lottie.dart';
 
 class Career extends StatefulWidget {
   @override
@@ -12,24 +15,26 @@ class _CareerState extends State<Career> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        padding: const EdgeInsets.only(top: 0),
-        height: 380,
-        width: double.infinity,
-        child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: const BoxDecoration(
-            color: Color(0xFF006FFD),
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(25),
-              bottomRight: Radius.circular(25),
-            ),
-          ),
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25), // Adjust radius if needed
+      ),
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xFF006FFD),
+          borderRadius: BorderRadius.all(Radius.circular(25)),
+        ),
+        padding: const EdgeInsets.all(16),
+        constraints: BoxConstraints(
+          maxWidth: 800,
+          // Adjust this value to fit your content
+        ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min, // Ensures dialog size fits content
+
             children: [
-              const SizedBox(height: 70),
+              const SizedBox(height: 20),
               const Padding(
                 padding: EdgeInsets.only(
                     left: 10.0), // Adjust the left padding as needed
@@ -37,7 +42,7 @@ class _CareerState extends State<Career> {
                   "Uncover Your Career Path",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 18,
                     fontFamily: 'Inter-semibold',
                   ),
                 ),
@@ -46,10 +51,10 @@ class _CareerState extends State<Career> {
               const Padding(
                 padding: EdgeInsets.only(left: 10.0),
                 child: Text(
-                  "Search and see where your future\nleads!",
+                  "Search and see where your future leads!",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 14,
+                    fontSize: 12,
                     fontFamily: 'Inter-regular',
                   ),
                 ),
@@ -61,7 +66,7 @@ class _CareerState extends State<Career> {
                   hintText: "Search Major",
                   hintStyle: TextStyle(
                     color: Colors.black.withOpacity(0.6),
-                    fontSize: 18,
+                    fontSize: 14,
                     fontFamily: 'Inter-regular',
                   ),
                   prefixIcon: const Icon(
@@ -76,6 +81,7 @@ class _CareerState extends State<Career> {
                       icon: const Icon(
                         Icons.send_rounded,
                         color: Color(0xFF000000),
+                        size: 20, // Adjust icon size if needed
                       ),
                       onPressed: () {
                         // Add the action you want to perform when the send icon is pressed
@@ -89,6 +95,8 @@ class _CareerState extends State<Career> {
                   ),
                   filled: true,
                   fillColor: const Color(0xFFEDEDED),
+                                  contentPadding: const EdgeInsets.symmetric(vertical: 10.0),
+
                 ),
                 onFieldSubmitted: (value) => _search(value, selectedCountry),
               ),
@@ -99,7 +107,7 @@ class _CareerState extends State<Career> {
                   "Country",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 20,
+                    fontSize: 18,
                     fontFamily: 'Inter-semibold',
                   ),
                 ),
@@ -117,9 +125,8 @@ class _CareerState extends State<Career> {
             ],
           ),
         ),
-      ),
-    );
-  }
+      );
+}
 
   Widget buildCountryButton(String country) {
     final isSelected = selectedCountry == country;
@@ -139,20 +146,141 @@ class _CareerState extends State<Career> {
         country,
         style: TextStyle(
           color: isSelected ? Color(0xFF006FFD) : Colors.white,
-          fontSize: 16,
+          fontSize: 12,
           fontFamily: 'Inter-semibold',
         ),
       ),
     );
   }
 
-void _search(String majorName, String country) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => CareerResult(majorName: majorName, country: country),
+ Future<Map<String, dynamic>> fetchCareerPath(String majorName, String country) async {
+    try {
+      final String url = "http://10.1.87.197:5000/";
+      final String api = url + "career-path-" + country.toLowerCase();
+      print("api : " + api);
+      final response = await http.post(
+        Uri.parse(api),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'career_name': majorName,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        return jsonDecode(response.body);
+      } else {
+        return {
+          'error': 'Failed to load Career path'
+        }; // Error message if API call fails
+      }
+    } catch (e) {
+      return {
+        'error': 'An error occurred'
+      }; // Error message if exception occurs
+    }
+  }
+
+void _search(String majorName, String country) async {
+    // Show the loading dialog
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) => Center(
+        child: Dialog(
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  'assets/icon/learning_loading.json', // Path to your Lottie animation file
+                  width: 100,
+                  height: 100,
+                  fit: BoxFit.fill,
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'Career Path is Generating\nAlmost Ready!',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontFamily: 'Inter-semibold',
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
+
+    // Fetch data
+    try {
+      final data = await fetchCareerPath(majorName, country);
+
+      // Dismiss the loading dialog
+      Navigator.of(context).pop();
+
+      // Check if data is empty or contains errors
+      if (data.containsKey('error')) {
+        // Show error message
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Error'),
+            content: Text(data['error']),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Dismiss the error dialog
+                },
+                child: Text('OK'),
+              ),
+            ],
+          ),
+        );
+        return; // Exit if there was an error
+      }
+
+      // Navigate to LearningResult with fetched data
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CareerResult(
+            majorName: majorName,
+            country: country,
+            data: data,
+          ),
+        ),
+      );
+    } catch (e) {
+      // Handle any unexpected exceptions
+      Navigator.of(context).pop(); // Dismiss the loading dialog
+
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text('An unexpected error occurred: $e'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Dismiss the error dialog
+              },
+              child: Text('OK'),
+            ),
+          ],
+        ),
+      );
+    }
   }
 
 }
--- lib/screens/CareerResult.dart
+++ lib/screens/CareerResult.dart
@@ -1,193 +1,115 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:lottie/lottie.dart';
+import 'package:majorwhisper/screens/Home.dart'; // Correct import for OnboardingScreen
 
-class CareerResult extends StatefulWidget {
+class CareerResult extends StatelessWidget {
   final String majorName;
   final String country;
+  final Map<String, dynamic> data;
 
-  CareerResult({required this.majorName, required this.country});
+  CareerResult(
+      {required this.majorName, 
+       required this.country, 
+       required this.data});
 
-  @override
-  _CareerResultState createState() => _CareerResultState();
-}
-
-class _CareerResultState extends State<CareerResult> {
-  Future<Map<String, dynamic>>? _CareerPathData;
-
-  @override
-  void initState() {
-    super.initState();
-    _CareerPathData = fetchCareerPath(widget.majorName);
-  }
-
-  Future<Map<String, dynamic>> fetchCareerPath(String majorName) async {
-    try {
-      final String url = "http://192.168.234.231:5000/";
-      final String api = url + "career-path-" + widget.country.toLowerCase();
-      print("api : " + api);
-      final response = await http.post(
-        Uri.parse(api),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-        body: jsonEncode(<String, String>{
-          'career_name': majorName,
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        return jsonDecode(response.body);
-      } else {
-        return {
-          'error': 'Failed to load Career path'
-        }; // Error message if API call fails
-      }
-    } catch (e) {
-      return {
-        'error': 'An error occurred'
-      }; // Error message if exception occurs
-    }
+  String toTitleCase(String text) {
+    return text
+        .toLowerCase()
+        .split(' ')
+        .map((word) =>
+            word.isEmpty ? '' : word[0].toUpperCase() + word.substring(1))
+        .join(' ');
   }
 
   @override
   Widget build(BuildContext context) {
-    // String capitalize(String text) {
-    //   return text
-    //       .split(' ')
-    //       .map(
-    //           (word) => word[0].toUpperCase() + word.substring(1).toLowerCase())
-    //       .join(' ');
-    // }
-
+    final imageUrl =
+        data['image_url']; // Use the image_url from the API response
+    final CareerPath =
+        data['career_path_' + country.toLowerCase()] ?? 'No data available';
     return Scaffold(
-      body: FutureBuilder<Map<String, dynamic>>(
-        future: _CareerPathData,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Lottie.asset(
-                    'assets/icon/career_loading.json', // Path to your Lottie animation file
-                    width: 150,
-                    height: 150,
-                    fit: BoxFit.fill,
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Career Path is Generating \nAlmost Ready!',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontFamily: 'Inter-semibold',
-                      color: Color(
-                          0xFF006FFD), // Customize the color to fit your app's theme
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+      body: Stack(
+        children: [
+          // Background Image in AppBar
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 300, // Height of AppBar
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: imageUrl != null && imageUrl.isNotEmpty
+                      ? NetworkImage(imageUrl)
+                      : AssetImage('assets/icon/profile_holder.png')
+                          as ImageProvider,
+                  fit: BoxFit.cover,
+                ),
               ),
-            );
-          } else if (snapshot.hasError || !snapshot.hasData) {
-            return Center(
-                child:
-                    Text('Error: ${snapshot.error ?? "Failed to load data"}'));
-          } else {
-            final data = snapshot.data!;
-            final imageUrl =
-                data['image_url']; // Use the image_url from the API response
-            final CareerPath =
-                data['career_path_' + widget.country.toLowerCase()] ??
-                    'No data available';
-
-            return Stack(
-              children: [
-                // Background Image in AppBar
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  child: Container(
-                    height: 300, // Height of AppBar
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: imageUrl != null && imageUrl.isNotEmpty
-                            ? NetworkImage(
-                                imageUrl) // Load from URL if available
-                            : AssetImage('assets/icon/profile_holder.png')
-                                as ImageProvider, // Load from assets if URL is not available
-                        fit: BoxFit.cover,
-                      ),
+              child: AppBar(
+                leading: IconButton(
+                  icon: Icon(
+                    Icons.arrow_back_ios_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Home()),
+                    );
+                  },
+                ),
+                title: Text(
+                  toTitleCase(majorName),
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+              ),
+            ),
+          ),
+          // Body content
+          Positioned(
+            top: 230,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Center(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50.0),
+                    topRight: Radius.circular(50.0),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      spreadRadius: 1,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
                     ),
-                    child: AppBar(
-                      leading: IconButton(
-                        icon: Icon(
-                          Icons.arrow_back_ios_rounded,
-                          color: Colors.white, // Set icon color to white
-                          size: 30, // Increase icon size
-                        ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                      ),
-                      title: Text(
-                        widget.majorName,
-                        style: TextStyle(
-                          fontSize: 30, // Increase font size
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white, // Bold text
-                        ),
-                      ),
-                      backgroundColor: Colors
-                          .transparent, // Make the AppBar background transparent
-                      elevation: 0, // Remove the shadow if not needed
+                  ],
+                ),
+                child: Markdown(
+                  data: CareerPath,
+                  styleSheet: MarkdownStyleSheet(
+                    p: TextStyle(
+                      fontSize: 13,
+                      fontFamily: 'Inter-regular',
                     ),
                   ),
                 ),
-                // Body content
-                Positioned(
-                  top:
-                      230, // Position body content to overlap slightly with AppBar
-                  left: 0,
-                  right: 0,
-                  bottom: 0,
-                  child: Center(
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(50.0),
-                          topRight: Radius.circular(50.0),
-                        ),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.1),
-                            spreadRadius: 1,
-                            blurRadius: 5,
-                            offset: Offset(0, 3), // changes position of shadow
-                          ),
-                        ],
-                      ),
-                      child: Markdown(
-                        data: CareerPath,
-                        styleSheet: MarkdownStyleSheet(
-                          p: TextStyle(
-                            fontSize: 13,
-                            fontFamily: 'Inter-regular',
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
-          }
-        },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
