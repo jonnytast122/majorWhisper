08bf57f282eaeda7736f80430f3ace28ed8bb51a
JamesChristos <ouksereiraksa123@gmail.com>
2024-09-21 10:52:53 +0700

    done with clipboard of copying sql code

--- lib/screens/CourseDetail.dart
+++ lib/screens/CourseDetail.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CourseDetail extends StatelessWidget {
   @override
@@ -59,7 +60,8 @@ class CourseDetail extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 20), // Add spacing between sections
-            Expanded( // Wrap ListView.builder in Expanded
+            Expanded(
+              // Wrap ListView.builder in Expanded
               child: ListView.builder(
                 itemCount: 1, // Number of items to display
                 itemBuilder: (context, index) {
@@ -78,8 +80,10 @@ class CourseDetail extends StatelessWidget {
                         ),
                       ],
                     ),
-                    child: Padding( // Add padding inside Expanded
-                      padding: const EdgeInsets.all(8.0), // Adjust padding as needed
+                    child: Padding(
+                      // Add padding inside Expanded
+                      padding:
+                          const EdgeInsets.all(8.0), // Adjust padding as needed
                       child: Row(
                         children: [
                           Expanded(
@@ -105,21 +109,83 @@ class CourseDetail extends StatelessWidget {
                                 ),
                                 SizedBox(height: 8.0),
                                 Container(
-                                  padding: const EdgeInsets.symmetric(
-                                    vertical: 16.0,
-                                    horizontal: 16.0,
-                                  ),
+                                  margin: EdgeInsets.all(0.0),
                                   decoration: BoxDecoration(
-                                    color: const Color.fromARGB(255, 0, 0, 0),
                                     borderRadius: BorderRadius.circular(10.0),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.1),
+                                        spreadRadius: 1,
+                                        blurRadius: 1,
+                                        offset: Offset(0, 1),
+                                      ),
+                                    ],
                                   ),
-                                  child: Text(
-                                    "SELECT *  FROM Employees;\n-- Retrieve All Columns from a Table:\n\nSELECT FirstName, LastName FROM Employees;\n--Retrieves only the FirstName, and LastName columns from the Employees table.",
-                                    style: const TextStyle(
-                                      fontFamily: "Spacemono-Regular",
-                                      fontSize: 10.0,
-                                      color: Color(0xFFFFFFFF),
-                                    ),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 2.0, horizontal: 0.0),
+                                        height: 34.0, // Set an explicit height
+                                        decoration: BoxDecoration(
+                                          color: Colors.grey[800],
+                                          borderRadius: BorderRadius.only(
+                                            topLeft: Radius.circular(10.0),
+                                            topRight: Radius.circular(10.0),
+                                          ),
+                                        ),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.end,
+                                          children: [
+                                            IconButton(
+                                              icon: Icon(Icons.copy,
+                                                  color: Colors.white,
+                                                  size: 10),
+                                              padding: EdgeInsets
+                                                  .zero, // Remove padding around the IconButton
+                                              constraints:
+                                                  BoxConstraints(), // Removes minimum size constraints
+                                              onPressed: () {
+                                                Clipboard.setData(ClipboardData(
+                                                    text:
+                                                        "SELECT * FROM Employees;\n-- Retrieve All Columns from a Table:\n\nSELECT FirstName, LastName FROM Employees;\n--Retrieves only the FirstName, and LastName columns from the Employees table."));
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  SnackBar(
+                                                    content: Text(
+                                                        "Code copied to clipboard!"),
+                                                    duration:
+                                                        Duration(seconds: 2),
+                                                  ),
+                                                );
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                      Container(
+                                        padding: EdgeInsets.symmetric(
+                                            vertical: 16.0, horizontal: 16.0),
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          borderRadius: BorderRadius.only(
+                                            bottomLeft: Radius.circular(10.0),
+                                            bottomRight: Radius.circular(10.0),
+                                          ),
+                                        ),
+                                        child: Text(
+                                          "SELECT * FROM Employees;\n-- Retrieve All Columns from a Table:\n\nSELECT FirstName, LastName FROM Employees;\n--Retrieves only the FirstName, and LastName columns from the Employees table.",
+                                          style: TextStyle(
+                                            fontFamily: "Spacemono-Regular",
+                                            fontSize: 10.0,
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
                                   ),
                                 ),
                               ],
